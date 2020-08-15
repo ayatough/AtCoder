c3e998c3d3e81f79693309bbcfc64e6a21176d89
ayatough <modest.simple.life@gmail.com>
2020-08-15 23:02:05 +0900

    submit (AC)

--- ABC175/E.py
+++ ABC175/E.py
@@ -6,15 +6,17 @@ for _ in range(K):
 
 
 def solve():
-    memo = [[[0] * 4 for _ in range(C+1)] for _ in range(R+1)]
+    memo = [[0] * 4 for _ in range(C+1)]
+    prev = [[0] * 4 for _ in range(C+1)]
     for r in range(1,R+1):
+        prev, memo = memo, prev
         for c in range(1,C+1):
             v = RC[r-1][c-1]
-            memo[r][c][0] = max(memo[r-1][c][3], memo[r][c-1][0])
-            memo[r][c][1] = max(memo[r][c][0] + v, memo[r][c-1][1])
-            memo[r][c][2] = max(memo[r][c][1], memo[r][c-1][1] + v, memo[r][c-1][2])
-            memo[r][c][3] = max(memo[r][c][2], memo[r][c-1][2] + v, memo[r][c-1][3])
-    return memo[-1][-1][3]
+            memo[c][0] = max(prev[c][3], memo[c-1][0])
+            memo[c][1] = max(memo[c][0] + v, memo[c-1][1])
+            memo[c][2] = max(memo[c][1], memo[c-1][1] + v, memo[c-1][2])
+            memo[c][3] = max(memo[c][2], memo[c-1][2] + v, memo[c-1][3])
+    return memo[-1][3]
 
 if __name__ == "__main__":
     print(solve())
