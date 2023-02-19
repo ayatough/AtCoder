eb42f6855f086a06de2d4f77e0d6cdfd9dd1d560
ayatough <modest.simple.life@gmail.com>
2023-02-20 08:42:09 +0900

    AC* (subtask 2)

--- template_90/005.py
+++ template_90/005.py
@@ -1,22 +1,38 @@
 import sys
 input = sys.stdin.readline
+from copy import copy
 
 N, B, K = map(int, input().split())
 C = list(map(int, input().split()))
 MOD = 10**9 + 7
 
-DP = [[0] * B for _ in range(N+1)]
-DP[0][0] = 1
+L = N.bit_length()
+A = [[[0] * B for _ in range(B)]]
 
-for i in range(N):
-    CD = [-1] * 10
-    d = pow(10, i, B)
+for i in range(B):
     for c in C:
-        CD[c] = (d * c) % B
-    for b in range(B):
-        for c in C:
-            d = CD[c]
-            m = (b+d)%B
-            DP[i+1][m] = (DP[i+1][m] + DP[i][b]) % MOD
+        m = (10*i + c) % B
+        A[0][i][m] += 1
 
-print(DP[-1][0])
+for _ in range(L):
+    cum = [[0] * B for _ in range(B)]
+    for i in range(B):
+        for j in range(B):
+            for k in range(B):
+                cum[i][j] += A[-1][i][k] * A[-1][k][j]
+                cum[i][j] %= MOD
+    A.append(cum)
+
+DP = [0] * B
+DP[0] = 1
+for i, bit in enumerate(bin(N)[2:][::-1]):
+    PP = [0] * B
+    if bit == "0":
+        continue
+    for j in range(B):
+        for k in range(B):
+            PP[j] += A[i][j][k] * DP[k]
+            PP[j] %= MOD
+    DP = copy(PP)
+
+print(DP[0])
